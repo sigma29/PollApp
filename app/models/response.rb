8c8be8a3cefd692c972c7edebf3cc1bd381d521b
leah and rj <leah+rj@gmail.com>
2015-08-13 14:19:27 -0400

    Add User#completed_polls.

--- app/models/response.rb
+++ app/models/response.rb
@@ -25,14 +25,14 @@ class Response < ActiveRecord::Base
 #ask why this fires twice
   def respondent_has_not_already_answered_question
     self.sibling_responses.each do |sibling|
-      if sibling.user_id = self.user_id
+      if sibling.user_id == self.user_id
         errors[:user_id] << "can't respond to the same question!"
       end
     end
   end
 
   def author_cant_respond_to_own_poll
-    if self.answer_choice.question.author.id = self.user_id
+    if self.answer_choice.question.author.id == self.user_id
       errors[:user_id] << "can't respond to your own poll"
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,45 @@ class User < ActiveRecord::Base
     through: :authored_questions,
     source: :answer_choices
 
-  private
 
+  def completed_polls
 
-
+    Poll.all
+      .joins("LEFT OUTER JOIN questions ON questions.poll_id = polls.id")
+      .joins("LEFT OUTER JOIN answer_choices ON answer_choices.question_id = questions.id")
+      .joins("LEFT OUTER JOIN (SELECT responses.* FROM responses WHERE responses.user_id = #{self.id}) AS user_responses ON user_responses.answer_choice_id = answer_choices.id")
+      .group("polls.id")
+      .select("polls.*")
+      .having("COUNT(user_responses.answer_choice_id) = COUNT(DISTINCT questions.id)")
+      # .select("polls.*")
+    #
+    #
+    # SELECT
+    #    polls.*
+    # FROM
+    #   polls
+    # JOIN
+    #   questions
+    # ON
+    #   questions.poll_id = polls.id
+    # LEFT OUTER JOIN
+    #   answer_choices
+    # ON
+    #   answer_choices.question_id = questions.id
+    # LEFT OUTER JOIN
+    #     (SELECT
+    #       responses.*
+    #      FROM
+    #       responses
+    #      WHERE
+    #       responses.user_id = 1
+    #     ) AS user_responses
+    # ON
+    #   user_responses.answer_choice_id = answer_choices.id
+    # GROUP BY
+    #   polls.id
+    # HAVING
+    #   COUNT(user_responses.answer_choice_id) = COUNT(DISTINCT questions.id)
+  end
 
 end
