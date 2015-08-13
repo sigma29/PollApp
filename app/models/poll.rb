b3ebb3e838322af17b1cd41ce08e8652e217dc93
leah and rj <leah+rj@gmail.com>
2015-08-13 09:00:58 -0400

    Add validation to User and Poll classes.

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,6 @@
 class Poll < ActiveRecord::Base
+  validates :title, :author_id, presence: true
+
   belongs_to :author,
     class_name: 'Author',
     foreign_key: :author_id,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :user_name, presence: true
+
   has_many :authored_polls,
     class_name: "Poll",
     foreign_key: :author_id,
